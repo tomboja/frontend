958b31cebd9ee13aa010e090dcc37d53ad086e17
Raymond Broome <rbroome@gmail.com>
2022-10-24 17:26:00 -0500

    update login component with roles

--- src/components/admin/LoginPage.js
+++ src/components/admin/LoginPage.js
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { validatePassword, validateDateOfBirth, validatePhone } from '../../utils/formUtils'
 import { setUserToken } from "../../reducers/loginReducer"
-import { STUDENT_LOGIN_TXT } from "../../texts"
+import { STUDENT_LOGIN_TXT, STUDENT_LOGIN_FORM_HEADING } from "../../texts"
 import '../../resources/styles/Login.css'
 
 export default function LoginPage() {
@@ -12,6 +12,7 @@ export default function LoginPage() {
     password: "",
   }
   const [userData, setUserData] = useState(initialState)
+  const roleOptions = ['ADMIN', 'STUDENT', 'REGISTRAR', 'FACULTY'];
 
   const setEmail = (e) => {
     const email = e.target.value;
@@ -27,33 +28,51 @@ export default function LoginPage() {
     })
   }
 
+  const setRole = (e) => {
+    const role = e.target.value;
+    setUserData((oldData) => {
+      return { ...oldData, role }
+    })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Input data: ', userData)
-    dispatch(setUserToken(userData.email))
+    dispatch(setUserToken(userData))
     setUserData(initialState)
   }
 
   return (
     <div className="container">
-      <h4>{STUDENT_LOGIN_TXT}</h4>
-      <form action="" onSubmit={handleSubmit} method="post" className="mx-auto border p-3 my-2">
-        <div className="row mb-3">
-          <label htmlFor="email" className="col-sm-2 col-form-label"><b>Email</b></label>
-          <div className="col-sm-10">
-            <input onChange={setEmail} className="form-control" type="email" placeholder="Email" name="email" required value={userData.email} />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <label htmlFor="psw" className="col-sm-2 col-form-label"><b>Password</b></label>
-          <div className="col-sm-10">
-            <input onChange={setPassword} className="form-control" type="password" placeholder="Password" name="psw" required value={userData.password} />
-          </div>
-        </div>
-
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
-        </div>
+        <h4>{STUDENT_LOGIN_TXT}</h4>
+        <form className="row align-items-center w-50 border" action="" onSubmit={handleSubmit} method="post">
+            <h4>{STUDENT_LOGIN_FORM_HEADING}</h4>
+            <div className="row mb-3">
+                <label htmlFor="email" className="col-2 col-form-label"><b>Email</b></label>
+                <div className="col-6">
+                    <input onChange={setEmail} className="form-control" type="email" placeholder="Email" name="email" required value={userData.email} />
+                </div>
+            </div>
+            <div className="row mb-3">
+                <label htmlFor="psw" className="col-2 col-form-label"><b>Password</b></label>
+                <div className="col-6">
+                    <input onChange={setPassword} className="form-control" type="password" placeholder="Password" name="psw" required value={userData.password} />
+                </div>
+            </div>
+            <div className="row mb-3">
+                <label htmlFor="role" className="col-2 col-form-label"><b>Role</b></label>
+                <div className="col-6">
+                    <select onChange={setRole} className="form-select" name="role" required>
+                        {roleOptions.map(role => <option key={role} value={role}>{role}</option>)}
+                    </select>
+                </div>
+            </div>
+            <div className="row text-end mb-3">
+                <div className="col-8">
+                    <button type="submit" className="btn btn-prime">Login</button>
+                </div>
+            </div>
+        </form>
     </div>
   )
 }
--- src/texts.js
+++ src/texts.js
@@ -5,6 +5,7 @@
 export const WELCOME_MESSAGE = 'Welcome to ABC University'
 export const STUDENT_REGISTRATION_TXT = 'MIU University Student Registration'
 export const STUDENT_LOGIN_TXT = 'Welcome to ABC University'
+export const STUDENT_LOGIN_FORM_HEADING = 'Login'
 export const ABC_UNIVERSITY = 'ABC University'
 export const COURSE_OFFERINGS = 'Course Offerings'
 export const ABOUT_US = 'About us'

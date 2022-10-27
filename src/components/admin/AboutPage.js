19f3a5de139baff430b6f5c3038554177aa6ebc5
Raymond Broome <rbroome@gmail.com>
2022-10-27 13:59:36 -0500

    Update about page

--- src/components/admin/AboutPage.js
+++ src/components/admin/AboutPage.js
@@ -4,12 +4,27 @@ const AboutPage = () => {
     <hr />
     <h2>Who are we?</h2>
     <p>
-    We share a strong commitment to personal inner growth, wellness, sustainability, and positive values.
-    We support a progressive and inclusive campus culture that is creative, dynamic, and focused.
+    ABC University is a private university located in Fairfield, Iowa. It was founded in 1974.
+    The university offers degree programs in art, business, education, communications, 
+    mathematical science, literature, and engineering.
     </p>
-    <p>
-    Graduate and receive a Masters Degree in Computer Science. Our school is amazing! 
-    </p>
+    <h2>Our Online System</h2>
+    <p>This online system provides the following capabilities:</p>
+    <h6>For Students</h6>
+    <ul>
+      <li>Apply for courses</li>
+      <li>Access Materials</li>
+      <li>Submit Assignments</li>
+      <li>Take tests, quizzes and exams periodically</li>
+      <li>Apply online for grade sheet</li>
+      <li>Take self-assessment tests</li>
+    </ul>
+    <h6>For Faculty</h6>
+    <ul>
+      <li>Generate the Grading reports</li>
+      <li>Assign quizzes, tests, and other homework assignments</li>
+      <li>Get a detailed report about a student</li>
+    </ul>
   </div>
 }
 
